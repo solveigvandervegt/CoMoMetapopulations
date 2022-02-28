Fixed bugs with network and initiating infections, and split propensity calculation into separate function.

Gillespie SEIR models/Gillespie_SEIR_multirun_main.py
@@ -39,18 +39,17 @@ number_of_runs = 5 # number of times we want to run the stochastic model
 
 store_totaltimetraces = []
 store_fractionInodes = []
-
-# build network
-print('Building network...')
-adjacency_matrix = build_powerlaw_network(n)
         
-# list of reactions
-print('Building list of reactions...')
-rxn = build_reaction_vector_list(n,c,adjacency_matrix)
-
 #%%
 for i in range(number_of_runs):
     print("Starting instance %d" %(i+1))
+    # build network
+    print('Building network...')
+    adjacency_matrix = build_powerlaw_network(n)
+    # list of reactions
+    print('Building list of reactions...')
+    rxn = build_reaction_vector_list(n,c,adjacency_matrix)
+    print('Starting simulation...')
     #t,x,em = single_model_run_SEIR(final_timepoint,total_pop,n,np.array([b,g,m,l,p]),adjacency_matrix)
     t,x = single_model_run_SEIR_eff(final_timepoint,total_pop,n,np.array([b,g,m,l,p]),adjacency_matrix,rxn)
     print("Finished simulation, processing output...")

Gillespie SEIR models/network_function.py
@@ -33,10 +33,9 @@ def build_powerlaw_network(n):
     adjacency_matrix = np.zeros((n,n)) # initiate edges matrix
     list_of_edges = [] # initiate list of all edges in network
     while len(list_of_stubs)>0:
-        print(list_of_stubs)
         if (len(list_of_stubs) == 2) & ((list_of_stubs[0] == list_of_stubs[1]) or (list_of_stubs in list_of_edges)): # cannot connect to own node
             # if fault in last edge, just start over
-            list_of_stubs = list_of_stubs_store
+            list_of_stubs = list_of_stubs_store.copy()
             adjacency_matrix = np.zeros((n,n)) # initiate edges matrix
             list_of_edges = [] # initiate list of all edges in network
         else :